bd55da743bb433ed623674c17499457a4b738ac6
Jagadish Rath <jrath95@gmail.com>
2009-05-11 17:46:02 +0530

    Algorithms added to find haufen keywords controller actions and views added to see keywords from all the algos for top 10 haufens of last hsession

--- app/controllers/haufens_controller.rb
+++ app/controllers/haufens_controller.rb
@@ -1,3 +1,21 @@
 class HaufensController < ApplicationController
 
+  def latest_top_ten
+    @hsession = Hsession.find(:first, 
+                              :order => "id DESC")
+    @haufens = Haufen.find(:all,
+                           :conditions => "haufens.hsession_id = #{@hsession.id}",
+                           :order      => "haufens.broadness DESC",
+                           :limit      => 10)
+    rawstories = []
+    story_ids = @haufens.collect{|h| h.members.strip.split(' ')}.flatten*','
+    rawstories = Rawstory.find(:all, :conditions => "id IN ( #{story_ids} )") unless story_ids.blank?
+    @rawstories_hashed = rawstories.group_by{|s| s.haufen_id}
+  end
+  def show
+    @haufen = Haufen.find(params[:id])
+    @rawstories = []
+    story_ids = @haufen.members.strip.split(' ')*','
+    @rawstories = Rawstory.find(:all, :conditions => "id IN ( #{story_ids} )") unless story_ids.blank?
+  end
 end
--- app/models/rawstory.rb
+++ app/models/rawstory.rb
@@ -13,52 +13,64 @@ class Rawstory < ActiveRecord::Base
   {:field => :created_at}]
 
   class << self
-    def find_en_keywords(text)
-      raw_text = text.gsub(/, |\. |“|”|; | - |_|\n+|\\|\"|\(|\)/, ' ')
-      ignore_kw = ['jan','feb','mar','apr','may','jun','jul', 'aug','sep','oct','nov','dec',    
+    def find_en_keywords(text, return_counts = false)
+      raw_text = text.gsub(/,|\.|“|”|;|:|\?|- |_|\n+|\\|\"|\(|\)/, ' ')
+      ignore_kw = ['jan','feb','mar','apr','may','jun','jul', 'aug','sep','oct','nov','dec',
+                   'januar','february','march','april','may','june','july', 'august','september','october','november','december', 
                    'monday','tuesday','wednesday','thursday','friday','saturday','sunday',
-                   'a','ap','about','an','am','and','are','at','as', 
-                   'be','but','by', 
+                   'first', 'second', 'third',
+                   'a','ap','about','an','am','and','are','at','as','also','after',
+                   'be','but','by','before', 
                    'can',
                    'do','does', 
                    'for','from','got', 
                    'has','have','he','her','him',
-                   'if','ii','in','into','is','it',
+                   'if','ii','in','into','is','it','its',
                    'no','not','now',
                    'of','off','on','out','over', 
                    'she','should','so','some', 
-                   'that','the','their','them','there','they','this','to',
+                   'that','the','their','them','there','they','this','to','too',
                    'under','up',
                    'you',
                    'was','we','what','when','which','who','will','with']
       raw_keywords = raw_text.split(/\s+/)
+      raw_keywords = raw_keywords.first(40)
       raw_keywords.reject! do |kw|
         ignore_kw.include?(kw.downcase)
       end
       count_hashed = raw_keywords.inject(Hash.new(0)) {|h,x| h[x]+=1;h}
-      sorted_keys  = count_hashed.keys.sort_by{|k| count_hashed[k]}
-      sorted_keys
+      sorted_keys  = count_hashed.keys.sort_by{|k| - count_hashed[k]}
+      if return_counts 
+        return sorted_keys, count_hashed
+      else
+        return sorted_keys
+      end
     end
-    def find_de_keywords(text)
-      raw_text = text.gsub(/, |\. |“|”|; | - |_|\n+|\\|\"|\(|\)/, ' ')
+    def find_de_keywords(text, return_counts = false)
+      raw_text = text.gsub(/,|\.|“|”|;|:|\?|- |_|\n+|\\|\"|\(|\)/, ' ')
       raw_keywords = raw_text.split(/\s+/)
+      raw_keywords = raw_keywords.first(40)
       raw_keywords.reject! do |kw|
         kw.match(/[A-Z]/).nil?
       end
       count_hashed = raw_keywords.inject(Hash.new(0)) {|h,x| h[x]+=1;h}
-      sorted_keys  = count_hashed.keys.sort_by{|k| count_hashed[k]}
-      sorted_keys
+      sorted_keys  = count_hashed.keys.sort_by{|k| - count_hashed[k]}
+      if return_counts 
+        return sorted_keys, count_hashed
+      else
+        return sorted_keys
+      end
     end
   end
   
-  def all_keywords
-    if defined?(@all_keywords)
-      return @all_keywords
-    end
+  def all_keywords(return_counts = false)
+    #if defined?(@all_keywords)
+    #  return @all_keywords
+    #end
     if self.language == 2
-      @all_keywords = Rawstory.find_de_keywords(self.title.to_s + ' ' + self.body.to_s)
+      @all_keywords = Rawstory.find_de_keywords(self.title.to_s + ' ' + self.body.to_s, return_counts)
     else
-      @all_keywords = Rawstory.find_en_keywords(self.title.to_s + ' ' + self.body.to_s)
+      @all_keywords = Rawstory.find_en_keywords(self.title.to_s + ' ' + self.body.to_s, return_counts)
     end
     return @all_keywords
   end
