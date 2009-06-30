efc67a82c31bb743bc934c438ee74ed2c21a15b4
Jagadish Rath <jrath95@gmail.com>
2009-06-30 10:23:13 +0530

    changes for email_alerts

--- db/migrate/019_daily_weekly_alerts.rb
+++ db/migrate/019_daily_weekly_alerts.rb
@@ -3,11 +3,12 @@ class DailyWeeklyAlerts < ActiveRecord::Migration
     add_column :users, :last_alert_at, :datetime
     add_column :users, :alert_type, :integer, :null => false, :default => User::Alert::IMMEDIATE # = 1
     User.find(:all).each do |u|
-      if u.alerts
-        u.alert_type = User::Alert::IMMEDIATE 
+      unless u.alerts
+        u.alert_type = User::Alert::OFF      
         u.save!
       end
     end
+
   end
 
   def self.down
